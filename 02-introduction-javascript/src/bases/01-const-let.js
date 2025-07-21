f7bc9102321aaa583a2165ead10df1b6c5b6f2c9
Aslly Zuñiga <asllyz2610@gmail.com>
2025-07-21 15:18:20 -0500

    include lessons on Variables and Constants, and Template Strings

---
+++ 02-introduction-javascript/src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+const nombre = "Aslly";
+const apellido = "Zuñiga";
+
+let valorDado = 4;
+valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  const nombre = "Daniel";
+  console.log(nombre);
+}
+console.log(valorDado);
