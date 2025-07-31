07a9244aba27e25c4f8aff540738bdece0efcc0d
Aslly Zuñiga <asllyz2610@gmail.com>
2025-07-30 20:47:32 -0500

    Add DefaultProps

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,9 +1,23 @@
-export const FirstApp = () => {
+import PropTypes from "prop-types";
+export const FirstApp = ({ title, subTitle, name }) => {
   return (
     <>
-      <h1>Aslly</h1>
+      <h1>{title}</h1>
       {/* <code>{JSON.stringify(newMessage)}</code> */}
-      <p>Zuñiga Chalapud</p>
+      <p>{subTitle}</p>
+      <p>{name}</p>
     </>
   );
 };
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number
+};
+
+FirstApp.defaultProps ={
+  title: 'No hay titulo',
+  subTitle: 'No hat subtitulo',
+  name: 'Aslly Zuñiga'
+
+}
