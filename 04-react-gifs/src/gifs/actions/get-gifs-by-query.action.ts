2db9017a862c14327642cb2cb1e84b4a8be1627e
Aslly Zuñiga <asllyz2610@gmail.com>
2025-09-09 18:59:35 -0500

    feat(api): add getGifsByQuery service for Giphy integration

--- 04-react-gifs/src/gifs/actions/get-gifs-by-query.action.ts
+++ 04-react-gifs/src/gifs/actions/get-gifs-by-query.action.ts
@@ -1,10 +1,10 @@
-import { giphyApi } from '../api/giphy.api';
+import { giphyApi } from "../api/giphy.api";
 
-import type { GiphyResponse } from '../interfaces/giphy.response';
-import type { Gif } from '../interfaces/gif.interface';
+import type { GiphyResponse } from "../interfaces/giphy.response";
+import type { Gif } from "../interfaces/gif.interface";
 
 export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
-  const response = await giphyApi<GiphyResponse>('/search', {
+  const response = await giphyApi<GiphyResponse>("/search", {
     params: {
       q: query,
       limit: 10,
