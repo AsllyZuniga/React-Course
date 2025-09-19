017db5dc8b9f43ae448afed7905beca403b5a192
Aslly Zuñiga <asllyz2610@gmail.com>
2025-09-18 19:04:11 -0500

    feat: add 06-react-hooks project

--- 05-react-ts-gifs/src/gifs/actions/get-gifs-by-query.action.ts
+++ 05-react-ts-gifs/src/gifs/actions/get-gifs-by-query.action.ts
@@ -1,16 +1,17 @@
-import { giphyApi } from '../api/giphy.api';
+import { giphyApi } from "../api/giphy.api";
 
-import type { GiphyResponse } from '../interfaces/giphy.response';
-import type { Gif } from '../interfaces/gif.interface';
+import type { GiphyResponse } from "../interfaces/giphy.response";
+import type { Gif } from "../interfaces/gif.interface";
 
 export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
-  const response = await giphyApi<GiphyResponse>('/search', {
+  const response = await giphyApi<GiphyResponse>("/search", {
     params: {
       q: query,
       limit: 10,
     },
   });
 
+  
   return response.data.data.map((gif) => ({
     id: gif.id,
     title: gif.title,
