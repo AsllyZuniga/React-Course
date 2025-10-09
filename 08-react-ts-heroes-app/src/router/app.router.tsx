af790b0f5a3a18ab0996a7cd95cee43df06698e5
Aslly Zuñiga <asllyz2610@gmail.com>
2025-10-09 00:02:38 -0500

    feat: configure appRouter with lazy-loaded SearchPage, hero routes, and admin layout using React Router

--- 08-react-ts-heroes-app/src/router/app.router.tsx
+++ 08-react-ts-heroes-app/src/router/app.router.tsx
@@ -1,15 +1,19 @@
-import { createBrowserRouter } from 'react-router';
+import { lazy } from "react";
+import { createBrowserRouter, Navigate } from "react-router";
 
-import { AdminLayout } from '@/admin/layouts/AdminLayout';
-import { AdminPage } from '@/admin/pages/AdminPage';
-import { HeroesLayout } from '@/heroes/layouts/HeroesLayout';
-import { HeroPage } from '@/heroes/pages/hero/HeroPage';
-import { HomePage } from '@/heroes/pages/home/HomePage';
-import { SearchPage } from '@/heroes/pages/search/SearchPage';
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { HeroPage } from "@/heroes/pages/hero/HeroPage";
+import { HomePage } from "@/heroes/pages/home/HomePage";
+
+// import { SearchPage } from '@/heroes/pages/search/SearchPage';
+
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HeroesLayout />,
     children: [
       {
@@ -17,18 +21,23 @@ export const appRouter = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'heroes/1',
+        path: "heroes/:idSlug",
         element: <HeroPage />,
       },
       {
-        path: 'search',
+        path: "search",
         element: <SearchPage />,
+      },
+      {
+        path: "*",
+        // element: <h1>404</h1>,
+        element: <Navigate to="/" />,
       },
     ],
   },
 
   {
-    path: '/admin',
+    path: "/admin",
     element: <AdminLayout />,
     children: [
       {
